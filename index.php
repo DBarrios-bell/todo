17c88ce253509c6e42c0d9ed8dd166ea20765cea
Devinson Barrios <devinson.barrios@gmail.com>
2023-08-31 09:48:57 -0500

    separando js del index

--- index.php
+++ index.php
@@ -13,19 +13,9 @@ require_once 'controlador.php';
     <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"> -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <link rel="stylesheet" type="text/css" href="style.css">
+    <script src="script.js"></script>
     <title>Todo</title>
-    <script type="text/javascript">
-        function tiempoReal() {
-            var tabla = $.ajax({
-                url: 'listTask.php',
-                dataType: 'text',
-                async: false
-            }).responseText;
 
-            document.getElementById("tableTask").innerHTML = tabla;
-        }
-        setInterval(tiempoReal, 1000);
-    </script>
 </head>
 <body>
     <div class="container">
@@ -50,28 +40,6 @@ require_once 'controlador.php';
             </div>
         </div>
     </div>
-    <script>
-        $("#enviar").click(function() {
-            $data = $("#task_id").serialize(); 
-            $datareal = $data.replace("task=",'')
-            
-            if ($datareal === null || $datareal === "") {
-                alert('Por favor escribir el detalle de la tarea');
-                return;
-            }
-            $.ajax({
-                url: "index.php",
-                type: "POST",
-                data: $("#task_id").serialize(),
-                success: function(task) {
-                    $("#ajax").html(task); 
-                }
-            });
-            alert('Guardado Con Exito');
-            return false;
-        });
-    
-    </script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
 </body>
---
+++ script.js
@@ -0,0 +1,31 @@
+
+function tiempoReal() {
+    var tabla = $.ajax({
+        url: 'listTask.php',
+        dataType: 'text',
+        async: false
+    }).responseText;
+
+    document.getElementById("tableTask").innerHTML = tabla;
+}
+setInterval(tiempoReal, 1000);
+
+$("#enviar").click(function() {
+    $data = $("#task_id").serialize(); 
+    $datareal = $data.replace("task=",'')
+    
+    if ($datareal === null || $datareal === "") {
+        alert('Por favor escribir el detalle de la tarea');
+        return;
+    }
+    $.ajax({
+        url: "index.php",
+        type: "POST",
+        data: $("#task_id").serialize(),
+        success: function(task) {
+            $("#ajax").html(task); 
+        }
+    });
+    alert('Guardado Con Exito');
+    return false;
+});
